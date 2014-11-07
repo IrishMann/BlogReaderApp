a52feda778b0ffa6619788dd4907f47acf8e654e
Isaiah Basu <isaiahbasu@gmail.com>
2014-11-07 11:43:07 -0800

    Done! With it all!
    Vid. 38 completed, lol. It's not l8 m8 :D

--- blogreader/BlogActivity.java
+++ blogreader/BlogActivity.java
@@ -1,10 +1,17 @@
 package android.basui.blogreader;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+
+import java.net.URI;
 
 
 public class BlogActivity extends Activity {
@@ -23,6 +30,19 @@ public class BlogActivity extends Activity {
         listView = (ListView)findViewById(R.id.ListView);
 
         listView.setEmptyView(progressBar);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.i("BlogActivity", "Title: " + BlogPostParser.get().posts.get(position));
+
+                Intent intent = new Intent(getApplicationContext(), BlogWebActivity.class);
+                Uri blogUri = Uri.parse(BlogPostParser.get().posts.get(position).url);
+                intent.setData(blogUri);
+
+                startActivity(intent);
+            }
+        });
 
 
 
--- blogreader/BlogPostAdapter.java
+++ blogreader/BlogPostAdapter.java
@@ -1,9 +1,12 @@
 package android.basui.blogreader;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 
@@ -17,7 +20,12 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost>{
         BlogPost post = getItem(position);
 
         if (convertView == null) {
-            convertView;
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         }
+
+        TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
+        textView.setText(post.title);
+
+        return convertView;
     }
 }
